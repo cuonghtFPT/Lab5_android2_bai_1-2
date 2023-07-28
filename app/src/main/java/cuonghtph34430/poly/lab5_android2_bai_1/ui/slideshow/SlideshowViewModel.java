4dceaca3edf39856d478d55131b7726b01a4357d
cuonghtph34430 <cuonghtph34430@fpt.edu.vn>
2023-07-28 18:01:17 +0700

    Initial commit

---
+++ app/src/main/java/cuonghtph34430/poly/lab5_android2_bai_1/ui/slideshow/SlideshowViewModel.java
@@ -0,0 +1,19 @@
+package cuonghtph34430.poly.lab5_android2_bai_1.ui.slideshow;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class SlideshowViewModel extends ViewModel {
+
+    private final MutableLiveData<String> mText;
+
+    public SlideshowViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is slideshow fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
